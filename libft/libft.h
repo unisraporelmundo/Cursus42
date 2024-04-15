b4f8ba581e84409b2ca2c26a95c4f787832b1257
Israel Izquierdo Garcia <iizquier@c1r12s3.42malaga.com>
2024-04-15 13:49:35 +0200

    libft

---
+++ libft/libft.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iizquier <iizquier@student.42malaga.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/15 13:21:22 by iizquier          #+#    #+#             */
+/*   Updated: 2024/04/15 13:42:23 by iizquier         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+#include <stdio.h>
+#include <unistd.h>
+
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isprint(int c);
+int		ft_isascii(int c);
+int		ft_isalnum(int c);
+
+#endif
