1e30419d55081569bdb6fe3c6f0dc03b32ebbd6b
Israel Izquierdo Garcia <iizquier@c1r15s3.42malaga.com>
2024-04-18 10:35:00 +0200

    funcioneslibft

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: iizquier <iizquier@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/15 13:21:22 by iizquier          #+#    #+#             */
-/*   Updated: 2024/04/15 13:42:23 by iizquier         ###   ########.fr       */
+/*   Updated: 2024/04/16 11:26:17 by iizquier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,5 +21,7 @@ int		ft_isdigit(int c);
 int		ft_isprint(int c);
 int		ft_isascii(int c);
 int		ft_isalnum(int c);
+int		ft_tolower(int c);
+int		ft_toupper(int c);
 
 #endif
