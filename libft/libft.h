743521f740e16ad4464e986253eb83ebe6715e93
Isra Izquierdo <theviking@MacBook-Pro-de-Isra.local>
2024-05-21 13:54:19 +0200

    FuncionesBonus

--- libft/libft.h
+++ libft/libft.h
@@ -18,6 +18,14 @@
 # include <stddef.h>
 # include <string.h>
 # include <stdlib.h>
+# include <stdint.h>
+# include <limits.h>
+
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}	t_list;
 
 int			ft_isalpha(int c);
 int			ft_isdigit(int c);
@@ -54,5 +62,13 @@ char		*ft_strtrim(const char *s1, const char *set);
 char		*ft_strmapi(const char *s, char (*f)(unsigned int, char));
 char		*ft_itoa(int n);
 char		**ft_split(const char *str, char c);
-
+t_list		*ft_lstnew(void *content);
+void		ft_lstadd_front(t_list **lst, t_list *new);
+int			ft_lstsize(t_list *lst);
+t_list		*ft_lstlast(t_list *lst);
+void		ft_lstadd_back(t_list **lst, t_list *new);
+void		ft_lstdelone(t_list *lst, void (*del)(void *));
+void		ft_lstclear(t_list **lst, void (*del)(void *));
+void		ft_lstiter(t_list *lst, void (*f)(void *));
+t_list		*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 #endif 
